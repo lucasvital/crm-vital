90ffb9dbb8b158acf074e6204b0b91baae2d0bc6
Lucas <lucasvitalsilva17@gmail.com>
2025-12-11 12:16:22 -0300

    feat: tags in deals

--- lib/custom_exceptions/open_ai_error.rb
+++ lib/custom_exceptions/open_ai_error.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class CustomExceptions::OpenAIError < CustomExceptions::Base
+class CustomExceptions::OpenAiError < CustomExceptions::Base
   def http_status
     422
   end
